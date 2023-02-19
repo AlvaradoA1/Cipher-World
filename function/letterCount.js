7521230ee7cc045c514fc2b84292d4e7925213a5
jameslu3 <106623560+jameslu3@users.noreply.github.com>
2023-02-18 23:53:38 -0500

    Delete letterCount.js
    delete redundant

--- function/letterCount.js
+++
@@ -1,5 +0,0 @@
-function letterCount(rightGuessString){
-    var result = [...rightGuessString].reduce((alphabets, i) => { alphabets[i] = alphabets[i] ? alphabets[i] + 1 : 1; return alphabets }, {});
-    return result
-}
-console.log(letterCount("HELLO ARE. YOU GOOD?"));
